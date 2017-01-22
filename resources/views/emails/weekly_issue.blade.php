7ac5d5a95b1b8eaad2094750794e7e9d6a522c71
Summer <cj@estgroupe.com>
2017-01-22 21:40:44 +0800

    issue title

--- resources/views/emails/weekly_issue.blade.php
+++ resources/views/emails/weekly_issue.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+    <title>{{ $issue->name }} | Laravel 资讯</title>
     <meta name="viewport" content="width=device-width">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
